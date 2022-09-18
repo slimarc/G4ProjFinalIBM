d789ff02c937fcd02af7edb0099f9d275fe534eb
cassialeaal <cassialeaal@gmail.com>
2022-09-18 15:23:02 -0300

    modificando ClienteDAO, ContaDAO e Endereco DAO para JpaRepository

--- src/main/java/br/com/hellobank/api/dao/ClienteDAO.java
+++ src/main/java/br/com/hellobank/api/dao/ClienteDAO.java
@@ -2,11 +2,11 @@ package br.com.hellobank.api.dao;
 
 import java.util.ArrayList;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.hellobank.api.model.entidades.Cliente;
 
-public interface ClienteDAO extends CrudRepository<Cliente, Integer> {
+public interface ClienteDAO extends JpaRepository<Cliente, Integer> {
     
     ArrayList<Cliente> findBynomeContaining(String nome);
     ArrayList<Cliente> findBycpf(String cpf);
--- src/main/java/br/com/hellobank/api/dao/EnderecoDAO.java
+++ src/main/java/br/com/hellobank/api/dao/EnderecoDAO.java
@@ -2,10 +2,10 @@ package br.com.hellobank.api.dao;
 
 import java.util.ArrayList;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.hellobank.api.model.entidades.Endereco;
 
-public interface EnderecoDAO extends CrudRepository<Endereco, Integer> {
+public interface EnderecoDAO extends JpaRepository<Endereco, Integer> {
     ArrayList<Endereco> findByCep(String cep);
 }
